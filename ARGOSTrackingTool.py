bfb9d83960c33a351323754f4d338ff64e167314
PierreMishra <prashank.mishra2011@gmail.com>
2020-09-23 02:00:25 -0400

    Use while loop to ingest file line

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,19 +16,28 @@ file_name = './data/raw/Sara.txt'
 file_object = open(file_name, 'r')
 
 #read contents of file into a list
-line_list = file_object.readlines()
+#line_list = file_object.readlines()
 
 #close file
-file_object.close()
+#file_object.close()
+
+#for while loop, read the first line in th file
+lineString = file_object.readline()
+
+#iterate using a while loop
+#while loop is much more memory efficient
+while lineString:
 
 # pretend we line one line of data from file
 #lineString = '20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0'
 #lineString = line_list[100]
 
 #iterate through all lines in the lineList
-for lineString in line_list:
+#for lineString in line_list:
     
     if lineString[0] in ("#","u"):
+        #read the next line
+        lineString = file_object.readline()
         continue
     
     #split the string into a list of data items
@@ -43,3 +52,9 @@ for lineString in line_list:
     
     #print the location
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon:{obs_long} on {obs_date}")
+    
+    #read the next line
+    lineString = file_object.readline()
+    
+#close the file
+file_object.close()
