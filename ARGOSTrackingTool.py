df7e8368d2966f367f7c786065e58fd3c1487799
PierreMishra <prashank.mishra2011@gmail.com>
2020-09-28 16:09:20 -0400

    Filters out lc not in 1,2,3

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,28 +16,32 @@ file_name = './data/raw/Sara.txt'
 file_object = open(file_name, 'r')
 
 #read contents of file into a list
-#line_list = file_object.readlines()
+line_list = file_object.readlines()
 
 #close file
-#file_object.close()
+file_object.close()
+
+#create two empty dictionary object
+date_dict = {}
+coord_dict = {}
 
 #for while loop, read the first line in th file
-lineString = file_object.readline()
+#lineString = file_object.readline()
 
 #iterate using a while loop
 #while loop is much more memory efficient
-while lineString:
+#while lineString:
 
 # pretend we line one line of data from file
 #lineString = '20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0'
 #lineString = line_list[100]
 
 #iterate through all lines in the lineList
-#for lineString in line_list:
+for lineString in line_list:
     
     if lineString[0] in ("#","u"):
         #read the next line
-        lineString = file_object.readline()
+        #lineString = file_object.readline()
         continue
     
     #split the string into a list of data items
@@ -47,14 +51,20 @@ while lineString:
     record_id = lineData[0]
     obs_date = lineData[2]
     obs_lc = lineData[4]
-    obs_lat = lineData[6]
-    obs_long = lineData[7]
     
-    #print the location
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon:{obs_long} on {obs_date}")
+#    if obs_lc not in ("1", "2", "3"):
+#        continue
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+    
+    #print the location if lc is 1, 2 or 3
+    if obs_lc in ("1", "2", "3"):
+        print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {obs_date}")
+        date_dict[record_id] = obs_date
+        coord_dict[record_id] = (obs_lat, obs_lon)
     
     #read the next line
-    lineString = file_object.readline()
+    #lineString = file_object.readline()
     
 #close the file
-file_object.close()
+#file_object.close()
